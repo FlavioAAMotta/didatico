71ef57319b91817d0352f526ec7a6f381891b93d
FlavioAAMotta <flavio.motta@sga.pucminas.br>
2022-10-27 16:59:33 -0300

    LP1

---
+++ LP1/API/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { Request, Response } from "express";
+import { AddressInfo } from "net";
+import cors from "cors";
+import dotenv from "dotenv";
+import {playlists} from "./playlists";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+const getAllPlaylists = async (req: Request, res: Response) => {
+  try {
+    const result = await playlists;
+    res.send(result);
+  } catch (error:any) {
+    res.send(error.message);
+  }
+};
+
+app.get("/playlists", getAllPlaylists);
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Server running at http://localhost:${address.port}`);
+  } else {
+    console.error(`Failure initializing server.`);
+  }
+});
---
+++ LP1/API/src/playlists.ts
@@ -0,0 +1,73 @@
+//Mocked playlists
+export const playlists = [
+    {
+        id: 1,
+        name: 'Rock',
+        owner: 'Flávio',
+        tracks: [
+            {
+                id: 1,
+                name: 'Highway to Hell',
+                artist: 'AC/DC',
+                album: 'Highway to Hell',
+                duration: 200,
+                url: 'https://www.youtube.com/watch?v=K4DyBUG242c'
+            },
+            {
+                id: 2,
+                name: 'Back in Black',
+                artist: 'AC/DC',
+                album: 'Back in Black',
+                duration: 200,
+                url: 'https://www.youtube.com/watch?v=pAgnJDJN4VA'
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: 'K-Pop',
+        owner: 'Nícollas',
+        tracks: [
+            {
+                id: 3,
+                name: 'Ddu-Du Ddu-Du',
+                artist: 'BLACKPINK',
+                album: 'BLACKPINK',
+                duration: 200,
+                url: 'https://www.youtube.com/watch?v=APn5MYJRTcM'
+            },
+            {
+                id: 4,
+                name: 'Kill This Love',
+                artist: 'BLACKPINK',
+                album: 'Kill This Love',
+                duration: 200,
+                url: 'https://www.youtube.com/watch?v=PHqXrZgKjx8'
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: 'Forróck',
+        owner: 'Nilton',
+        tracks: [
+            {
+                id: 5,
+                name: 'Trem Bala',
+                artist: 'Aviões do Forró',
+                album: 'Trem Bala',
+                duration: 200,
+                url: 'https://www.youtube.com/watch?v=Z9Y6XZQX8qU'
+            },
+            {
+                id: 6,
+                name: 'Camarote',
+                artist: 'Aviões do Forró',
+                album: 'Camarote',
+                duration: 200,
+                url: 'https://www.youtube.com/watch?v=Z9Y6XZQX8qU'
+            }
+        ]
+    }
+];
+
