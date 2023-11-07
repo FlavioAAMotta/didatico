46b8414788e86ce55125d4211e835bf55730b218
Flávio Andrade Amaral Motta <flavio.motta@sga.pucminas.br>
2023-11-07 18:20:38 -0300

    071123

---
+++ Front1/2023-2/JS/ManipulandoDOM/script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("change-button")
+const intros = document.getElementsByClassName("intro")
+
+const main = document.querySelector('#main-content');
+console.log(main)
+console.log(main.innerHTML)
+console.log(main.textContent)
