ac59a67c7e0f02883f612aa433a39744a859a758
FlavioAAMotta <flavio.motta@sga.pucminas.br>
2024-02-23 13:34:10 -0300

    Solução matrizes

---
+++ ED/2024/Vetores e Matrizes/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+   public static void main(String[] args){
+      int[][] matriz1 = {
+      {1,2},
+      {3,4}
+      };
+      int[][] matriz2 = {
+      {5,6},
+      {7,8}
+      };
+      somarMatrizes(matriz1, matriz2);
+   }
+}
