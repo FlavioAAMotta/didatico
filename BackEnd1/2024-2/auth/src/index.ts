c932f6dd8f3104a7c40c53d0e1616e9e51681608
FlavioAAMotta <flavio.motta@sga.pucminas.br>
2024-10-21 10:12:11 -0300

    att

--- BackEnd1/2024-2/auth/src/index.ts
+++ BackEnd1/2024-2/auth/src/index.ts
@@ -1,9 +1,9 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express, { Request, Response } from "express";
 import cors from "cors";
 
 import { AddressInfo } from "net";
-import {connection} from "./connection";
+import { connection } from "./connection";
 
 const app = express();
 
@@ -18,12 +18,11 @@ app.get("/", async (req: Request, res: Response) => {
   }
 });
 
-
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Server is running in http://localhost:\${address.port}`);
   } else {
-    console.error(\`Failure upon starting server.\`);
+    console.error("Failure upon starting server.");
   }
 });
