28a7c6ead45eb7b76ab05d1e48a6ab5d18a51ab6
FlavioAAMotta <flavioaam@hotmail.com>
2026-03-17 20:08:11 -0300

    att

--- front2/2026-1/Exercicios/listas/src/components/Lista.jsx
+++ front2/2026-1/Exercicios/listas/src/components/Lista.jsx
@@ -1,19 +1,26 @@
 import { useState } from "react"
 
-export const Lista  = () =>{
+export const Lista = () => {
     const [tarefa, setTarefa] = useState("")
     const [listaTarefa, setListaTarefa] = useState([])
 
-    const onChangeTarefa = (event) =>{
+    const onChangeTarefa = (event) => {
         setTarefa(event.target.value);
     }
 
-    const criarTarefa = (event) =>{
+    const criarTarefa = (event) => {
         event.preventDefault()
-        console.log("Criando tarefa")
         const listaTarefaRenderizada = [...listaTarefa];
         listaTarefaRenderizada.push(tarefa);
         setListaTarefa(listaTarefaRenderizada)
+    }
+
+    const removerItem = (tarefa) => {
+        const listaTarefaRenderizada = [...listaTarefa];
+        const listaFiltrada = listaTarefaRenderizada.filter((tarefaVetor) => {
+            return tarefaVetor != tarefa;
+        })
+        setListaTarefa(listaFiltrada)
     }
 
     return <>
@@ -21,9 +28,14 @@ export const Lista  = () =>{
         <form onSubmit={criarTarefa}>
             <input onChange={onChangeTarefa} placeholder="Digite a tarefa" value={tarefa}></input>
             <button>Criar</button>
-            <div>
-                {listaTarefa}
-            </div>
         </form>
+        <div class="lista-container">
+            {listaTarefa.map((tarefa, index) => {
+                return <div key={index} className="item-container">
+                    <p>{tarefa}</p>
+                    <div className="remove" onClick={() => removerItem(tarefa)}>X</div>
+                </div>
+            })}
+        </div>
     </>
 }
