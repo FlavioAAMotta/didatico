f1e4ad34cfdb0cf00be0b2d05c21fb892e58d96e
FlavioAAMotta <flavioaam@hotmail.com>
2026-05-25 21:37:35 -0300

    att

---
+++ Front1/2026/06 - JS/Condicionais_Lacos/atividade.js
@@ -0,0 +1,58 @@
+let valor = 1000;
+
+console.log("Ele ganhou", valor, "reais");
+console.log("Ele ganhou " + valor + " reais");
+console.log(`Ele ganhou ${valor} reais`);
+
+
+// let contador = 0;
+
+// //Toda vez que uso o comando ++, ele é o ultimo a executar da linha.
+
+// if(contador++ == 1){
+//     console.log("Entrou");
+// }
+// console.log(++contador);
+
+// console.log(2 == "2"); // Quando uso dois iguais, comparo apenas valor
+// console.log(2 === "2");// Quando uso tres iguais, comparo tanto valor quanto tipo
+
+// console.log(2 != "2"); // false
+// console.log(2 !== "2");// true
+
+// // // Exercício 34
+// // // Crie um programa que verifica se um número é igual a zero.
+
+// // let numero = 0;
+
+// // if(numero == 0){
+// //     console.log("Número Igual a Zero");
+// // }
+
+// // // Exercício 44
+// // // Crie um programa que calcula desconto:
+// // // compra menor que 100: sem desconto;
+// // // de 100 a 499: 10% de desconto;
+// // // 500 ou mais: 20% de desconto.
+
+// // let compra = 124;
+
+// // if(compra<100){
+// //     console.log("sem desconto");
+// // }else if(compra <= 499){
+// //     let desconto = compra * 0.1;
+// //     console.log("Desconto de: ", desconto);
+// // }else{
+// //     let desconto = compra * 0.2;
+// //     console.log("Desconto de: ", desconto);
+// // }
+
+// // // Exercício 62
+// // // Conte quantos números pares existem de 1 a 50.
+// // let quantidadePares = 0;
+// // for(let i = 1; i <=50; i++){
+// //     if(i % 2 == 0){
+// //         quantidadePares++;
+// //     }
+// // }
+// // console.log('Temos ', quantidadePares, ' números pares de 1 a 50');
